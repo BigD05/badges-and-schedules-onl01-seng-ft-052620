09fd4d1247ee3788053698b84f2f4156adb6e818
kyle Dornan <kyledornan26@gmail.com>
2020-05-31 00:43:51 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -19,7 +19,7 @@ def assign_rooms(attendees)
 end
 
 def printer(attendees)
-  batch_badge_creator(attendees)array.each do |id|
+  batch_badge_creator(attendees).each do |id|
     puts id
   end 
   
