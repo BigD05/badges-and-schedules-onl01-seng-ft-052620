7404293a74ce1261c122db915c98ed07f07dba87
kyle Dornan <kyledornan26@gmail.com>
2020-05-31 00:25:37 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,28 @@
-# Write your code here.
+def badge_maker (name)
+  return  "Hello, my name is #{name}."
+  end 
+  
+  def batch_badge_creator(attendees)
+attendees.collect do |name|
+    "Hello, my name is #{name}."
+ end
+ end
+      
+def assign_rooms(attendees)
+  counter = 1
+  room_assignments = []
+  attendees.each do |name|
+    room_assignments.push("Hello, #{name}! You'll be assigned to room #{counter}!")
+    counter += 1
+     end
+     room_assignments
+end
+
+def printer(atendees)
+  
+  attendees.each do |name|
+    puts batch_badge_creator
+    puts assign_rooms
+    return "Hello, #{name} you are assigned to room number #{num}"
+end
+end
